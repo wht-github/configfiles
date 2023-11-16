bb954db6d8fff688b700e54bfee6750d6e18ce11
Victor Wang <1479486567@qq.com>
2023-11-16 17:40:41 +0800

    posh config

---
+++ pwsh_profile.ps1
@@ -0,0 +1,13 @@
+# Import-Module oh-my-posh # 引入 oh-my-posh
+oh-my-posh init pwsh --config "~/.my_posh_theme.omp.json" | Invoke-Expression
+Import-Module posh-git # 引入 posh-git
+$env:POSH_GIT_ENABLED = $true
+# function Set-PoshGitStatus {
+#     $global:GitStatus = Get-GitStatus
+#     $env:POSH_GIT_STRING = $(Write-GitStatus -Status $global:GitStatus).Trim()
+# }
+# New-Alias -Name 'Set-PoshContext' -Value 'Set-PoshGitStatus' -Scope Global -Force
+# Set-Theme Powerlevel10k-Lean 
+# Set-PSReadlineKeyHandler -Key Tab -Function Complete # 设置 Tab 键补全
+# Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward # 设置向上键为后向搜索历史记录
+# Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward # 设置向下键为前向搜索历史纪录
