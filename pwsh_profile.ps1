9e6c6a9fec7b96ed32a47cab7d41e1af60a81091
wht-github <vicowang6567@qq.com>
2024-03-30 19:54:07 +0800

    add alias

--- pwsh_profile.ps1
+++ pwsh_profile.ps1
@@ -1,17 +1,12 @@
-# Import-Module oh-my-posh # 引入 oh-my-posh
 oh-my-posh init pwsh --config "~/.my_posh_theme.omp.json" | Invoke-Expression
 Import-Module posh-git # 引入 posh-git
 $env:POSH_GIT_ENABLED = $true
 function wt_admin {
     Start-Process wt "-d $(Get-Location)" -Verb RunAs
 }
+function _refreshenv {
+    $Env:PATH = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+}
 Set-Alias -Name "wt-admin" -Value wt_admin
-# function Set-PoshGitStatus {
-#     $global:GitStatus = Get-GitStatus
-#     $env:POSH_GIT_STRING = $(Write-GitStatus -Status $global:GitStatus).Trim()
-# }
-# New-Alias -Name 'Set-PoshContext' -Value 'Set-PoshGitStatus' -Scope Global -Force
-# Set-Theme Powerlevel10k-Lean 
-# Set-PSReadlineKeyHandler -Key Tab -Function Complete # 设置 Tab 键补全
-# Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward # 设置向上键为后向搜索历史记录
-# Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward # 设置向下键为前向搜索历史纪录
+Set-Alias -Name "ls" -Value lsd
+Set-Alias -Name "refreshenv" -Value _refreshenv
