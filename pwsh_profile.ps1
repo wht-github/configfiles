7a86548dad1f391765656f34ee7e74f6796520c2
wht-github <vicowang6567@qq.com>
2024-03-30 20:25:34 +0800

    upd

--- pwsh_profile.ps1
+++ pwsh_profile.ps1
@@ -1,12 +1,14 @@
 oh-my-posh init pwsh --config "~/.my_posh_theme.omp.json" | Invoke-Expression
 Import-Module posh-git # 引入 posh-git
 $env:POSH_GIT_ENABLED = $true
-function wt_admin {
+function Start-Admin {
     Start-Process wt "-d $(Get-Location)" -Verb RunAs
 }
-function _refreshenv {
+function Update-Env {
     $Env:PATH = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 }
-Set-Alias -Name "wt-admin" -Value wt_admin
 Set-Alias -Name "ls" -Value lsd
-Set-Alias -Name "refreshenv" -Value _refreshenv
+Set-Alias -Name "wt-admin" -Value Start-Admin
+Set-Alias -Name "refreshenv" -Value Update-Env
+
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
