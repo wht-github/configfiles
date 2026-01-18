8c047a7839f01e505a376045a9de11e690497c2e
wht-github <vicowang6567@qq.com>
2026-01-18 23:37:13 +0800

    upd

--- containers/nb-container/nb.ps1
+++ containers/nb-container/nb.ps1
@@ -2,4 +2,5 @@
 # Helper script to run nb via docker-compose
 
 # Since the entrypoint is now bash, we explicitly call the nb command
-docker compose run --rm nb nb $args
+# --service-ports is required to map ports (like 6789) when using 'run'
+docker compose run --rm --service-ports nb nb $args
